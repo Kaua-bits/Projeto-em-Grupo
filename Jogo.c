cb5c892472514ad2886a5469315296ac83cac0bf
Kaua Teo <kauateodossantos@gmail.com>
2025-11-15 18:48:11 -0300

    completo

---
+++ Jogo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "bib.h"
+
+int main(){
+    
+    unsigned int seed = (unsigned int)(time(NULL) ^ (uintptr_t) &seed);
+    srand(seed);
+
+    puts("\n---------------------------------- Jogo Perguntado ----------------------------------\n\n ----- Voce possui 01 vida, assim como 01 de cada acao especial para lhe ajudar -----\n");
+
+    Jogo *jogo = malloc(sizeof(Jogo));
+    inicia_jogo(jogo);
+
+    Pergunta *perg = malloc(sizeof(Pergunta));
+    inicia_pergunta(perg);
+
+    Grupo_de_Perguntas *grupo = malloc(sizeof(Grupo_de_Perguntas));
+    grupo->perguntas = malloc(sizeof(Pergunta*)*1);
+    populaGrupo(perg, jogo, grupo);
+    
+    ler_pergunta(jogo,1);
+    
+    free_tudo(jogo, perg, grupo);
+    
+    return 0;
+}
